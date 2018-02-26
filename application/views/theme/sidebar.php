cd10f97cb630fc1f2a28cc7ffd3dcfdd132b6f9f
Your Name <dikfajar06@gmail.com>
2018-02-26 14:49:01 +0700

    update barang

--- application/views/theme/sidebar.php
+++ application/views/theme/sidebar.php
@@ -93,16 +93,38 @@
                         <b class="arrow"></b>
                     </li>
 
-                    <li class="">
-                        <a href="widgets.html">
-                            <i class="menu-icon fa fa-list-alt"></i>
+    <li class="">
+                        <a href="#" class="dropdown-toggle">
+                            <i class="menu-icon fa fa-list"></i>
                             <span class="menu-text"> Barang </span>
+
+                            <b class="arrow fa fa-angle-down"></b>
                         </a>
 
                         <b class="arrow"></b>
+
+                        <ul class="submenu">
+                            <li class="">
+                                <a href="tables.html">
+                                    <i class="menu-icon fa fa-caret-right"></i>
+                                    Stok Barang
+                                </a>
+
+                                <b class="arrow"></b>
+                            </li>
+
+                            <li class="">
+                                <a href="jqgrid.html">
+                                    <i class="menu-icon fa fa-caret-right"></i>
+                                    Beli Barang
+                                </a>
+
+                                <b class="arrow"></b>
+                            </li>
+                        </ul>
                     </li>
 
-                     <li class="">
+                                        <li class="">
                         <a href="widgets.html">
                             <i class="fa fa-edit fa-fw"></i>
                             <span class="menu-text"> Catalog Display </span>
