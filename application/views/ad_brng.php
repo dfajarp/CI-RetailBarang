25b94f8cfae30e7c13368c753ee8de0c3a2c4c10
Your Name <dikfajar06@gmail.com>
2018-03-01 16:08:41 +0700

    update beli barang

--- application/views/ad_brng.php
+++ application/views/ad_brng.php
@@ -1,54 +1,87 @@
-<div class="col-md-4" id="form">
-    <div class="widget-box">
-        <div class="widget-header">
-            <h4 class="widget-title">Barang 1</h4>
+                            <div class="col-md-4" id="form"">
+                                <div class="widget-box">
+                                    <div class="widget-header">
+                                        <h4 class="widget-title">Barang</h4>
 
-            <div class="widget-toolbar">
-                <a href="#" data-action="collapse">
-                    <i class="ace-icon fa fa-chevron-up"></i>
-                </a>
+                                        <div class="widget-toolbar">
+                                            <a href="#" data-action="collapse">
+                                                <i class="ace-icon fa fa-chevron-up"></i>
+                                            </a>
 
-                <a href="#" data-action="close">
-                    <i class="ace-icon fa fa-times"></i>
-                </a>
-            </div>
-        </div>
+                                            <a href="#" class="close-btn" data-action="close">
+                                                <i class="ace-icon fa fa-times"></i>
+                                            </a>
+                                        </div>
+                                    </div>
 
-        <div class="widget-body">
-            <div class="widget-main">
-                <div>
-                    <label for="form-field-8">ID Barang</label>
+                                    <div class="widget-body">
+                                        <div class="widget-main">
+                                            <div>
+                                                <label for="form-field-8">ID Barang</label>
 
-                    <input class="form-control" id="form-field-8">
-                </div>
+                                                <input class="form-control" id="form-field-8">
+                                            </div>
 
-                <hr />
+                                         
 
-                <div>
-                    <label for="form-field-9">Nama Barang</label>
+                                            <div>
+                                                <label for="form-field-9">Nama Barang</label>
 
-                    <input class="form-control limited" id="form-field-9" maxlength="50">
-                </div>
+                                                <input class="form-control limited" id="form-field-9" maxlength="50">
+                                            </div>
 
-                <hr />
+                                            
 
-                <div>
-                    <label for="form-field-11">Harga Barang</label>
+                                            <div>
+                                                <label for="form-field-11">Harga Barang</label>
 
-                    <input id="form-field-11" class="autosize-transition form-control">
-                </div>
+                                                <input id="form-field-11" class="autosize-transition form-control">
+                                            </div>
 
-                <hr />
+                                        
 
-                <div>
-                    <label for="form-field-11">Jumlah Barang</label>
+                                            <div>
+                                                <label for="form-field-11">Jumlah Barang</label>
 
-                    <input id="form-field-11" class="autosize-transition form-control">
-                </div>
+                                                <input id="form-field-11" class="autosize-transition form-control">
+                                            </div>
 
+                                            <div class="widget-body">
+                                                <div class="widget-main">
+                                                    <div class="form-group">
+                                                        <div class="col-xs-12">
+                                                           <input type="file" class="id-input-file-2" />
+                                                        </div>
+                                                    </div>
 
+                                                </div>
+                                            </div>
 
-            </div>
-        </div>
-    </div>
-</div><!-- /.span -->
+                                        </div>
+                                    </div>
+
+                                </div>
+
+                            </div>
+<script>
+	$(function(){
+
+		$('.id-input-file-2').ace_file_input({
+            no_file:'No File ...',
+            btn_choose:'Choose',
+            btn_change:'Change',
+            droppable:false,
+            onchange:null,
+            thumbnail:false //| true | large
+            //whitelist:'gif|png|jpg|jpeg'
+            //blacklist:'exe|php'
+            //onchange:''
+            //
+        });
+
+		$('.close-btn').on("click", function(){
+			$(this).parent().parent().parent().parent().hide();
+		});
+
+    });
+ </script>
--- application/views/barang/vtambah.php
+++ application/views/barang/vtambah.php
@@ -68,74 +68,8 @@
                             </div>
 
                             <div class="hr hr-24"></div>
-                            <div class="col-md-4" id="form"">
-                                <div class="widget-box">
-                                    <div class="widget-header">
-                                        <h4 class="widget-title">Barang 1</h4>
-
-                                        <div class="widget-toolbar">
-                                            <a href="#" data-action="collapse">
-                                                <i class="ace-icon fa fa-chevron-up"></i>
-                                            </a>
-
-                                            <a href="#" data-action="close">
-                                                <i class="ace-icon fa fa-times"></i>
-                                            </a>
-                                        </div>
-                                    </div>
-
-                                    <div class="widget-body">
-                                        <div class="widget-main">
-                                            <div>
-                                                <label for="form-field-8">ID Barang</label>
-
-                                                <input class="form-control" id="form-field-8">
-                                            </div>
-
-                                            <hr />
-
-                                            <div>
-                                                <label for="form-field-9">Nama Barang</label>
-
-                                                <input class="form-control limited" id="form-field-9" maxlength="50">
-                                            </div>
-
-                                            <hr />
-
-                                            <div>
-                                                <label for="form-field-11">Harga Barang</label>
-
-                                                <input id="form-field-11" class="autosize-transition form-control">
-                                            </div>
-
-                                            <hr />
-
-                                            <div>
-                                                <label for="form-field-11">Jumlah Barang</label>
-
-                                                <input id="form-field-11" class="autosize-transition form-control">
-                                            </div>
-
-                                            <div class="widget-body">
-                                                <div class="widget-main">
-                                                    <div class="form-group">
-                                                        <div class="col-xs-12">
-                                                           <input type="file" id="id-input-file-2" />
-                                                        </div>
-                                                    </div>
-
-                                                </div>
-                                            </div>
-
-                                        </div>
-                                    </div>
-
-                                </div>
-
-                            </div>
-
-
                         </form>
+
                     </div>
                     <div class="clearfix form-actions">
                         <div class="col-md-offset-3 col-md-9">
@@ -155,7 +89,6 @@
                         </div>
                     </div>
 
-                    <div class="hr hr-24"></div>
                 </div>
             </div>
         </div>
@@ -165,8 +98,7 @@
 
 
 
-<script src="<?= base_url() ?>/assets/js/ace-elements.min.js"></script>
-        <script src="<?= base_url() ?>/assets/js/ace.min.js"></script>
+
 <script>
 
     $(document).ready(function () {
@@ -177,18 +109,18 @@
             frm++;
         });
 
-        $('#id-input-file-1 , #id-input-file-2').ace_file_input({
-            no_file:'No File ...',
-            btn_choose:'Choose',
-            btn_change:'Change',
-            droppable:false,
-            onchange:null,
-            thumbnail:false //| true | large
-            //whitelist:'gif|png|jpg|jpeg'
-            //blacklist:'exe|php'
-            //onchange:''
-            //
-        });
+        // $('#id-input-file-1 , #id-input-file-2').ace_file_input({
+        //     no_file:'No File ...',
+        //     btn_choose:'Choose',
+        //     btn_change:'Change',
+        //     droppable:false,
+        //     onchange:null,
+        //     thumbnail:false //| true | large
+        //     //whitelist:'gif|png|jpg|jpeg'
+        //     //blacklist:'exe|php'
+        //     //onchange:''
+        //     //
+        // });
     });
 </script>
 
