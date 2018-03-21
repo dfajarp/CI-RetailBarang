3951baed7026f84b4f5988c0bb77d4bb79516ad9
Your Name <dikfajar06@gmail.com>
2018-03-21 08:24:41 +0700

    error

--- application/models/m_katalog.php
+++ application/models/m_katalog.php
@@ -1,27 +1,11 @@
 <?php
 
 class M_katalog extends CI_Model{
-	function tampil($where)
+	function tampil()
 	{
 		//$data = $this->db->get('katalog');
 		//return $data->result();
-		return $this->db->get_where('katalog', $where);
+		return $this->db->get('katalog');
 	}
 
-	function input($data,$table)
-	{
-		$this->db->insert($table,$data);
-	}
-
-	function hapus($where,$table)
-	{
-		$this->db->where($where);
-		$this->db->delete($table);
-	}
-
-	function update($where,$data,$table)
-	{
-		$this->db->where($where);
-		$this->db->update($table,$data);
-	}
 }
